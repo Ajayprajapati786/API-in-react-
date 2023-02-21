e6e9fd930b9918a847a0785e0ecfb227a1e7b103
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-21 09:58:01 +0530

    using async and await by own

--- src/App.js
+++ src/App.js
@@ -4,29 +4,12 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
-
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films')
-    .then((response)=>{
-      return response.json();
-    })
-    .then((data)=>{
+  async function fetchMoviesHandler(){
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
       const transformedMovies = data.results.map(movieData =>{
         return {
           id:movieData.episode_id,
@@ -36,7 +19,9 @@ function App() {
         }
       })
       setMovies(transformedMovies);
-    })
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
